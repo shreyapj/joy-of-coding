3c61cfe826bebfee2cbd41724fac2090d11c2e17
shreyapj <85942199+shreyapj@users.noreply.github.com>
2021-10-07 20:32:43 +0530

    Add files via upload

---
+++ size_of_datatype.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+   printf("the size of short int is %ld\n",sizeof(short int));
+   printf("the size of int is %ld\n",sizeof(int));
+   printf("the size of long int is %ld\n",sizeof(long int));
+   printf("the size of float is %ld\n",sizeof(float));
+   printf("the size of double is %ld\n",sizeof(double));
+   printf("the size of char is %ld\n",sizeof(char));
+   return 0;
+}
+
+
